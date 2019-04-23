fb9e0488a6c941d43b6f9e91a6166ac4d9b09a37
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-04-23 14:28:29 +0800

    benchmark

--- uncommenter_test.go
+++ uncommenter_test.go
@@ -40,3 +40,37 @@ func TestRemoveComment(t *testing.T) {
 		}
 	}
 }
+
+//BenchmarkRemoveComment-6   	  200000	     10003 ns/op
+func BenchmarkRemoveComment(b *testing.B) {
+	js := struct{ in, out string }{
+		`
+{
+    "seeds": [
+        "https://www.baidu.com"// http下载
+//
+      // "https://v.qq.com"
+    ]
+}`, `
+{
+    "seeds": [
+        "https://www.baidu.com"
+    ]
+}`}
+	for n := 0; n < b.N; n++ {
+
+		reader := strings.NewReader(js.in)
+		newReader := RemoveComment(reader)
+
+		bs, err := ioutil.ReadAll(newReader)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		out := string(bs)
+		if out != js.out {
+			b.Fatal("want:", js.out, "got: ", out)
+		}
+
+	}
+}
